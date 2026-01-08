0ab262a798a2730548cc9e4dd5865e54f8de0f2d
Yohan-ParkGist <68223714+Yohan-ParkGist@users.noreply.github.com>
2026-01-08 16:48:55 +0900

    hello

--- test.py
+++ test.py
@@ -1,10 +1,25 @@
 import sys
 
 n, m = map(int, sys.stdin.readline().split())
-word_freq = {}
-for _ in range(n):
-    word = sys.stdin.readline().rstrip()
-    if len(word)>=m:
-        word_freq[word]+=1
-        
-print(word_freq)
+
+def generate_combinations_4(n, k):
+    results = []
+    combination = []
+    def backtrack(start):
+        if len(combination) == k:
+            results.append(combination[:])
+            return 
+
+        for i in range(start, n+1):
+            combination.append(i)
+            backtrack(i)
+            combination.pop()
+
+
+    backtrack(1)
+    return results
+
+final_result = generate_combinations_4(n, m)
+
+for line in final_result:
+    print(*line)
