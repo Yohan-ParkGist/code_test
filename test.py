df15a63faa7b47fbfdeed13fb4f5879ab9b70a13
Yohan-ParkGist <68223714+Yohan-ParkGist@users.noreply.github.com>
2026-01-15 17:54:52 +0900

    hello world

--- test.py
+++ test.py
@@ -1,15 +1,32 @@
 import sys
 
-word1 = sys.stdin.readline().rstrip()
-word2 = sys.stdin.readline().rstrip()
-n = len(word1)
-m = len(word2)
-lcs = [[0]*(n+1) for _ in range(m+1)]
-for i in range(1, m+1):
-    for j in range(1, n+1):
-        if word1[j-1] == word2[i-1]:
-            lcs[i][j] = lcs[i-1][j-1]+1
-        else:
-            lcs[i][j] = max(lcs[i-1][j], lcs[i][j-1])
-    
-print(lcs[m][n])
+def matrix_mul(matrix1, matrix2):
+    ans = [[0]*dim for _ in range(dim)]
+    for i in range(dim):
+        for j in range(dim):
+            for l in range(dim):
+                ans[i][j] += matrix1[i][l]*matrix2[l][j]
+                ans[i][j] = ans[i][j]%p
+    return ans
+
+def mat_power(a, b):
+    if b == 1:
+        for i in range(dim):
+            for j in range(dim):
+                a[i][j] %= p
+        return a
+    temp = (mat_power(a, b//2))
+    if b%2 == 0:
+        return matrix_mul(temp, temp)
+    else:
+        return matrix_mul(a, matrix_mul(temp, temp))
+
+dim = 2
+n = int(sys.stdin.readline())
+p = 1000000007
+
+matrix = [[1, 1], [1, 0]]
+
+final = mat_power(matrix, n)
+
+print(final[0][1])
