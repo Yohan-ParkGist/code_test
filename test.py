029fae2002d0dbd72586e57b001221340200302c
Yohan-ParkGist <68223714+Yohan-ParkGist@users.noreply.github.com>
2026-01-23 10:28:51 +0900

    hello world

--- test.py
+++ test.py
@@ -1,31 +1,35 @@
 import sys
-from collections import deque
+import heapq
 
-def topology_sort():
-    global queue
-    result = []
-    for i in range(1, v+1):
-        if in_degree[i] == 0:
-            queue.append(i)
-            
-    while queue:
-        current = queue.popleft()
-        result.append(current)
-        for edge in directed_edge[current]:
-            in_degree[edge]-=1
-            if in_degree[edge] == 0:
-                queue.append(edge)
-                    
-    return result
-    
+def bellman_ford(init, n):
+    s = []
+    distance = [float('inf')]*(n+1)
+    distance[init] = 0
+    heapq.heappush(s, (0, init))
+    while s:
+        dist, u = heapq.heappop(s)
+        if distance[u] < dist:
+            continue
+        
+        for edge in edge_weight[u]:
+            cost = dist+edge[1]
+            if distance[edge[0]] > cost:
+                distance[edge[0]] = cost
+                heapq.heappush(s, (cost, edge[0]))
+                
+    return distance
+
+
 
 v, e = map(int, sys.stdin.readline().split())
-directed_edge = [[] for _ in range(v+1)]
-in_degree = [0]*(v+1)
+edge_weight = [[] for _ in range(v+1)]
+distance = [float('inf')]*(v+1)
+k = int(sys.stdin.readline())
 for _ in range(e):
-    a, b= map(int, sys.stdin.readline().split())
-    directed_edge[a].append(b)
-    in_degree[b]+=1
-    
-queue = deque()
-print(*topology_sort())
+    a, b, w = map(int, sys.stdin.readline().split())
+    edge_weight[a].append((b, w))
+
+bellman_ford(k, v)
+
+for i in range(1, v+1):
+    print(distance[i])
