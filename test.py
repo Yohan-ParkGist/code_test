f13acfa86539058a8da81d68411db751cf07ce0c
Yohan-ParkGist <68223714+Yohan-ParkGist@users.noreply.github.com>
2026-01-30 13:22:48 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,17 @@
 import sys
 
-n = int(sys.stdin.readline())
+n, m = map(int, sys.stdin.readline().split())
+memory = [0] + list(map(int, sys.stdin.readline().split()))
+cost = [0] + list(map(int, sys.stdin.readline().split()))
 
-r, c = map(int, sys.stdin.readline().split())
-rank = []
-rank.append(r)
-rank.append(c)
+dp = [[float('inf')]*(m+1) for _ in range(n+1)]
 
-for _ in range(n-1):
-    r, c = map(int, sys.stdin.readline().split())
-    rank.append(r)
-    rank.append(c)
-
-dp = [[[0]*(n+1) for _ in range(n+1)]]
-
-for diagonal in range(2, n+1):
-    for i in range(1, n-diagonal+2):
-        j = i + diagonal - 1
-        for p in range(i, j):
-            dp[i][j] = min()
+for i in range(1, n+1):
+    dp[i][0] = 0
+    for j in range(1, m+1):
+        if memory[i] >= j:
+            dp[i][j] = min(dp[i-1][j], cost[i])
+        else:
+            dp[i][j] = min(dp[i-1][j], dp[i-1][j-memory[i]]+cost[i])
+            
+print(dp[n][m])
