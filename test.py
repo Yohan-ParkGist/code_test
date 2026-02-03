4eb97eef7fe989e2d21872735b79aeb21b631073
Yohan-ParkGist <68223714+Yohan-ParkGist@users.noreply.github.com>
2026-02-03 16:13:17 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,39 @@
 import sys
+from collections import deque
 
-n, m = map(int, sys.stdin.readline().split())
-memory = [0] + list(map(int, sys.stdin.readline().split()))
-cost = [0] + list(map(int, sys.stdin.readline().split()))
+t = int(sys.stdin.readline())
 
-dp = [[float('inf')]*(m+1) for _ in range(n+1)]
-
-for i in range(1, n+1):
-    dp[i][0] = 0
-    for j in range(1, m+1):
-        if memory[i] >= j:
-            dp[i][j] = min(dp[i-1][j], cost[i])
-        else:
-            dp[i][j] = min(dp[i-1][j], dp[i-1][j-memory[i]]+cost[i])
-            
-print(dp[n][m])
+for _ in range(t):
+    n = int(sys.stdin.readline())
+    card = deque(map(int, sys.stdin.readline().split()))
+    
+    a = 0
+    b = 0
+    left_temp = card.popleft()
+    right_temp = card.pop()
+    turn = 0
+    
+    for _ in range(2*n):
+        if turn == 0:
+            turn = 1
+            if left_temp > right_temp:
+                a+=left_temp
+                if card:
+                    left_temp = card.popleft()
+            else:
+                a+=right_temp
+                if card:
+                    right_temp = card.pop()
+                
+        elif turn == 1:
+            turn = 1
+            if left_temp > right_temp:
+                b+=left_temp
+                if card:
+                    left_temp = card.popleft()
+            else:
+                b+=right_temp
+                if card:
+                    right_temp = card.pop()
+    
+    print(a)
